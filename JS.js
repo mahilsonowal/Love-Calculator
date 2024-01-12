34e7ab93679580d3d6aacdbb507b505f0ace3c9a
mahilsonowal <155747136+mahilsonowal@users.noreply.github.com>
2024-01-12 18:29:48 +0530

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,11 @@
+prompt("Enter your name : ");
+prompt("Enter your crush name : ");
+var n = Math.random();
+var no = Math.floor(n*100) + 1;
+
+if(no>60) {
+    alert("Love percentage : " + no + "%. Congratulation, you have a bright future!!");
+  }
+  else {
+      alert("Love percentage : " + no + "%. You need to work on yourself!!");
+  }
